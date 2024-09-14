c72b19e3369cec2b1aa1477dbeb1aaf220c76d7c
Antony Kivai <tkivai@outlook.com>
2024-09-14 21:59:05 +0300

    added purchase and purchase_detail table

--- presentation.sql
+++ presentation.sql
@@ -3,7 +3,7 @@ CREATE DATABASE spare_parts_shop;
 USE spare_parts_shop;
 
 CREATE TABLE parts (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY,
     name VARCHAR(100),
     category INT,
     manufacturer INT,
@@ -12,12 +12,24 @@ CREATE TABLE parts (
 );
 
 CREATE TABLE category (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY,
     name VARCHAR(100)
 );
 
 CREATE TABLE manufacturer (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY,
     name VARCHAR(100),
     warranty_period INT
+);
+
+create TABLE purchase (
+    id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    date DATE
+);
+
+create TABLE purchase_detail (
+    purchase_id INT,
+    part_id INT,
+    quantity INT
 );
