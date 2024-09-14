38e7a7494c294ea588d0bf4b59234b325b90341f
Antony Kivai <tkivai@outlook.com>
2024-09-14 21:28:16 +0300

    added category and manufacturer table

--- presentation.sql
+++ presentation.sql
@@ -5,9 +5,19 @@ USE spare_parts_shop;
 CREATE TABLE parts (
     id INT PRIMARY KEY NOT NULL,
     name VARCHAR(100),
-    category VARCHAR(100),
-    manufacturer VARCHAR(100),
+    category INT,
+    manufacturer INT,
     stock INT,
-    price DECIMAL, # In Ksh
-    warranty INT
+    price DECIMAL # In Ksh
+);
+
+CREATE TABLE category (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(100)
+);
+
+CREATE TABLE manufacturer (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(100),
+    warranty_period INT
 );
