d5dc264e5854189b2d9b7e59936010f6c11bb171
Antony Kivai <tkivai@outlook.com>
2024-09-14 19:52:07 +0300

    created a table for spare parts with all its relevant info

--- presentation.sql
+++ presentation.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE spare_parts_shop;
 
 USE spare_parts_shop;
+
+CREATE TABLE parts (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(100),
+    category VARCHAR(100),
+    manufacturer VARCHAR(100),
+    stock INT,
+    price DECIMAL, # In Ksh
+    warranty INT
+);
