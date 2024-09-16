6e55fdb8c68e66a36f796da7c698341be7bf2dbe
Antony Kivai <tkivai@outlook.com>
2024-09-16 13:31:54 +0300

    added foreign key constraints

--- presentation.sql
+++ presentation.sql
@@ -5,31 +5,38 @@ USE spare_parts_shop;
 CREATE TABLE parts (
     id INT PRIMARY KEY,
     name VARCHAR(100),
-    category INT,
-    manufacturer INT,
+    category_id INT,
+    manufacturer_id INT,
     stock INT,
-    price DECIMAL # In Ksh
+    price DECIMAL, # In Ksh
+#   -------------ADDITIONAL CONSTRAINTS-----------------------
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(id)
 );
 
-CREATE TABLE category (
+CREATE TABLE categories (
     id INT PRIMARY KEY,
     name VARCHAR(100)
 );
 
-CREATE TABLE manufacturer (
+CREATE TABLE manufacturers (
     id INT PRIMARY KEY,
     name VARCHAR(100),
     warranty_period INT
 );
 
-create TABLE purchase (
+create TABLE purchases (
     id INT PRIMARY KEY,
     customer_name VARCHAR(100),
     date DATE
 );
 
-create TABLE purchase_detail (
+create TABLE purchase_details (
     purchase_id INT,
     part_id INT,
-    quantity INT
+    quantity INT,
+#   ----------ADDITIONAL CONSTRAINTS------------------
+    PRIMARY KEY (purchase_id, part_id),
+    FOREIGN KEY (purchase_id) REFERENCES purchases(id),
+    FOREIGN KEY (part_id) REFERENCES parts(id)
 );
